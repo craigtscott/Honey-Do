f81e7d78d01e42822fd5a5375ccbd2bbfb0177a7
Craig Scott <craigtscottjr@gmail.com>
2017-01-10 15:17:51 -0800

    Fix broken link for the docs

---
+++ Honey_Do/frontend/honey_do.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Honey Do</h1>, root);
+});
