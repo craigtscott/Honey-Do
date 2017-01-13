9626e866bb7f70605a479c0eb8cc282c012f902c
Craig Scott <craigtscottjr@gmail.com>
2017-01-12 16:52:18 -0800

    Adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!([
+  {first_name: "Craig", last_name: "Scott", user_name: "cts85", password_digest: "$2a$10$66FZMpyhBbcz0wo9uGhTU.WvSEoBNRWI8E.wQbppkTWz58AJXdWAW", session_token: "To49IedHjbXUJYWan3Szog"},
+  {first_name: "Katarina", last_name: "Rossi", user_name: "dischorde", password_digest: "$2a$10$R/A68UU7WHoPntH3pVm5YeLXY6NfGWBUbXsVlhTejn8K7VxWv8lby", session_token: "aa_DZk6dsSJDQRmkzYlsAA"},
+  {first_name: "Jeff", last_name: "Gronewold", user_name: "tank_top", password_digest: "$2a$10$iEX7KCrcFmY6db.MXgWjfOhba3I4YjJQgApAyRfvNLPGBimwbLj0G", session_token: "sE_jIrg29ucaj--BSNBDfQ"},
+  {first_name: "Scot", last_name: "Mosher", user_name: "scck", password_digest: "$2a$10$Z30ySsIs7NQNf96woV7opOpVkw38mtDtdC8Fmg3uQjp6nCP8Mi9le", session_token: "dQ0Z5fepxlYqHGDySgYC7A"},
+  {first_name: "Guest", last_name: "demo", user_name: "demo", password_digest: "$2a$10$GxhDn1rE9/KP1Cyh.l4V1efWvAVXz8E138KUGHbZuYT17AY.Ct9xq", session_token: "Lb-yAGpNfwZSkZTUffkbbw"}
+])
